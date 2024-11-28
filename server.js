638e175a1cd02d9f345c35e9c8ca26341a1276d8
Matthew Rice <matthewcrice@yahoo.com>
2024-11-28 13:11:43 -0800

    file upload test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 
 // Create an Express app and listen for incoming requests on port 3000
@@ -38,3 +39,31 @@ app.use(router);
 app.listen(port, () => {
   console.log(`Server running at https://localhost:${port}/`);
 });
+*/
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload[0].filepath;
+      console.log(oldpath);
+      var newpath = __dirname + "\\" + files.filetoupload[0].originalFilename;
+      console.log(newpath);
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(3000);
